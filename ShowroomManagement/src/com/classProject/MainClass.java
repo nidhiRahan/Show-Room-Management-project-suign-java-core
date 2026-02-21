278e060af0f3cc1246c006ff5c4007402a7f75e1
NIDHI RAHANGDALE <127111855+nidhiRahan@users.noreply.github.com>
2026-02-21 10:28:12 +0530

    Update MainClass.java

--- ShowroomManagement/src/com/classProject/MainClass.java
+++ ShowroomManagement/src/com/classProject/MainClass.java
@@ -2,108 +2,106 @@ package com.classProject;
 
 import java.util.Scanner;
 
-interface utility{
-	 public void get_detail();
-	 public void set_detail();
- }
+interface Utility {
+    void get_detail();
+    void set_detail();
+}
+
 public class MainClass {
-	public static void mainMenu() {
-		System.out.println("******************Welcome to ShowRoom Management System****************************");
-        System.out.println();
-		System.out.println("===========================*******Enter your choice*******=====================");
-        System.out.println();
-        System.out.println("1].ADD SHOWROOM \t\t\t 2].ADD EMPLOYEE \t\t\t 3]. ADD CAR");
-	System.out.println();
-	System.out.println("4].GET SHOWROOM \t\t\t 5].GET EMPLOYEE \t\t\t 6]. ADD CAR");
-	System.out.println();
-	System.out.println("===============================================********Enter 0 to exist***************==============================================");
-	
-	}
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		Showroom[] showroom=new Showroom[5];
-		Employee[] employe=new Employee[5];
-		Car[] car=new Car[5];
-		int showroom_counter=0;
-		int car_counter=0;
-		int employee_counter=0;
-		int choice=100;
-		while(choice!=0) {
-			mainMenu();
-			choice=sc.nextInt();
-			while(choice!=9 && choice !=0) {
-				switch(choice) {
-				case 1:
-					showroom[showroom_counter]=new Showroom();
-				showroom[showroom_counter].set_detail();
-				showroom_counter++;
-				System.out.println();
-				System.out.println("1].ADD NEW SHOWROOM");
-				System.out.println("9].GO BACK TO MAIN MENU");
-				choice=sc.nextInt();
-				break;
-				
-				case 2:
-					employe[employee_counter]=new Employee();
-					employe[employee_counter].set_detail();
-					employee_counter++;
-					System.out.println();
-					System.out.println("2].ADD NEW EMPLOYEE");
-					System.out.println("9].GO BACK TO MAIN MENU");
-					choice=sc.nextInt();
-					break;
-					
-				case 3:
-					car[car_counter]=new Car();
-					car[car_counter].set_detail();
-					car_counter++;
-					System.out.println();
-					System.out.println("3].ADD NEW CAR");
-					System.out.println("9]. GO BACK TO MAIN MENU");
-					choice=sc.nextInt();
-					break;
-					
-				case 4:
-					for(int i=0; i<showroom_counter; i++) {
-						showroom[i].get_detail();
-						System.out.println();
-						System.out.println();
-					}
-					System.out.println();
-					System.out.println("9].GO BACK TO MAIN MENU");
-					System.out.println("0].EXIST");
-					choice=sc.nextInt();
-					break;
-				case 5: 
-					for(int i=0; i<employee_counter; i++) {
-						employe[i].get_detail();
-						System.out.println();
-						System.out.println();
-						break;
-					}
-					System.out.println();
-					System.out.println("9].GO BACK TO MAIN MENU");
-					System.out.println("0].Exist");
-					choice=sc.nextInt();
-					break;
-				case 6:
-					for(int i=0; i<car_counter; i++) {
-						car[i].get_detail();
-						System.out.println();
-						System.out.println();
-					}
-					System.out.println();
-					System.out.println("9].GO BACK TO MAIN MENU");
-					System.out.println("0]. EXIST");
-					choice=sc.nextInt();
-					break;
-					default:
-						System.out.println("ENTER VALID CHOICE");
-						break;
-				}}
-		}
-		
-	}
-	}
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void mainMenu() {
+        System.out.println("\n******** Welcome to Showroom Management System ********");
+        System.out.println("1. Add Showroom");
+        System.out.println("2. Add Employee");
+        System.out.println("3. Add Car");
+        System.out.println("4. Get Showroom Details");
+        System.out.println("5. Get Employee Details");
+        System.out.println("6. Get Car Details");
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    public static void main(String[] args) {
+
+        Showroom[] showroom = new Showroom[5];
+        Employee[] employee = new Employee[5];
+        Car[] car = new Car[5];
+
+        int showroomCount = 0;
+        int employeeCount = 0;
+        int carCount = 0;
+
+        int choice;
+
+        do {
+            mainMenu();
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    if (showroomCount < showroom.length) {
+                        showroom[showroomCount] = new Showroom();
+                        showroom[showroomCount].set_detail();
+                        showroomCount++;
+                    } else {
+                        System.out.println("Showroom storage full!");
+                    }
+                    break;
+
+                case 2:
+                    if (employeeCount < employee.length) {
+                        employee[employeeCount] = new Employee();
+                        employee[employeeCount].set_detail();
+                        employeeCount++;
+                    } else {
+                        System.out.println("Employee storage full!");
+                    }
+                    break;
+
+                case 3:
+                    if (carCount < car.length) {
+                        car[carCount] = new Car();
+                        car[carCount].set_detail();
+                        carCount++;
+                    } else {
+                        System.out.println("Car storage full!");
+                    }
+                    break;
+
+                case 4:
+                    for (int i = 0; i < showroomCount; i++) {
+                        showroom[i].get_detail();
+                        System.out.println("----------------------");
+                    }
+                    break;
+
+                case 5:
+                    for (int i = 0; i < employeeCount; i++) {
+                        employee[i].get_detail();
+                        System.out.println("----------------------");
+                    }
+                    break;
+
+                case 6:
+                    for (int i = 0; i < carCount; i++) {
+                        car[i].get_detail();
+                        System.out.println("----------------------");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Exiting system... Thank you!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+
+        } while (choice != 0);
+    }
+}
 
 
