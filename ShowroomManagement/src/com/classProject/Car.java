d858fbfe6f23e2014219bc65d0482949c440f43c
NIDHI RAHANGDALE <127111855+nidhiRahan@users.noreply.github.com>
2026-02-21 10:26:41 +0530

    Update Car.java

--- ShowroomManagement/src/com/classProject/Car.java
+++ ShowroomManagement/src/com/classProject/Car.java
@@ -2,39 +2,52 @@ package com.classProject;
 
 import java.util.Scanner;
 
+public class Car implements Utility {
 
+    private String carName;
+    private String carColor;
+    private String carFuelType;
+    private int carPrice;
+    private String carType;
+    private String carTransmission;
 
-public class Car extends Showroom implements utility {
-String car_name;
-String car_color;
-String car_fuel_type;
-int car_price;
-String car_type;
-String car_transmission;
+    @Override
+    public void set_detail() {
 
-@Override
-public void get_detail() {
-	System.out.println("NAME"+car_name);
-	System.out.println("COLOR:"+car_color);
-	System.out.println("FUEL TYPE"+car_fuel_type);
-	System.out.println("PRICE"+car_price);
-	System.out.println("CAR TYPE:"+ car_type);
-	System.out.println("TRANSMISSION"+ car_transmission);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n========== ENTER CAR DETAILS ==========");
+
+        System.out.print("Car Name: ");
+        carName = sc.nextLine();
+
+        System.out.print("Car Color: ");
+        carColor = sc.nextLine();
+
+        System.out.print("Fuel Type: ");
+        carFuelType = sc.nextLine();
+
+        System.out.print("Car Type (SUV/Sedan/etc): ");
+        carType = sc.nextLine();
+
+        System.out.print("Car Price: ");
+        carPrice = sc.nextInt();
+        sc.nextLine();  // Fix for nextLine issue
+
+        System.out.print("Transmission (Manual/Automatic): ");
+        carTransmission = sc.nextLine();
+    }
+
+    @Override
+    public void get_detail() {
+
+        System.out.println("\n--------- CAR DETAILS ---------");
+        System.out.println("Name        : " + carName);
+        System.out.println("Color       : " + carColor);
+        System.out.println("Fuel Type   : " + carFuelType);
+        System.out.println("Type        : " + carType);
+        System.out.println("Price       : " + carPrice);
+        System.out.println("Transmission: " + carTransmission);
+    }
 }
-public void set_detail() {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("=====================================******ENTER CAR DETAILS*****=========================================");
-	System.out.println("CAR NAME");
-	car_name=sc.nextLine();
-	System.out.println("CAR COLOR");
-	car_color=sc.nextLine();
-	System.out.println("CAR FUEL TYPE");
-	car_fuel_type=sc.nextLine();
-	System.out.println("CAR PRICE");
-    car_price=sc.nextInt();
-    System.out.println("CAR TRANSMISSION");
-    car_transmission=sc.nextLine();
-    total_car_in_stock++;
-}
-	
-}
+
